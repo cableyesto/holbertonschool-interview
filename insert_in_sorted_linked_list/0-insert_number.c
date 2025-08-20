50bf158efb1bd8ab2c99112c312d72735eca07da
cableyesto <dialkins@tutamail.com>
2025-08-20 12:03:54 +0200

    Project Insert in sorted linked list, task 0. Insert in sorted linked list.

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - insert node in a sorted linked list
+ * @head: pointer to pointer of first node of listint_t list
+ * @number: integer to be included in new node
+ * Return: adress of the new node or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+    if (head == NULL)
+	return (NULL);
+
+    listint_t *new;
+    listint_t *current;
+    listint_t *ptr_to_keep;
+    current = *head;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+	return (NULL);
+
+    new->n = number;
+    new->next = NULL;
+
+    while (current->next != NULL && current->next->n < number)
+	current = current->next;
+
+    ptr_to_keep = current->next;
+    new->next = ptr_to_keep;
+
+    current->next = new;
+
+    return (new);
+}
