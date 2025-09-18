3caf1a1344dd1e6911e864f97dc684c965bcd2a7
cableyesto <dialkins@tutamail.com>
2025-09-18 09:23:56 +0200

    Task 0. Linked list cycle done.

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to list to be freed
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+	listint_t *head;
+	int res = 0;
+
+	if (list == NULL)
+		return (res);
+
+	head = list;
+	current = list;
+
+	do {
+		if (current->next == head)
+		{
+			res = 1;
+			return (res);
+		}
+		current = current->next;
+	} while (current->next != NULL);
+
+	return (res);
+}
