e83dd592de789031ce4e7bf8b1642b66e023ef0f
cableyesto <dialkins@tutamail.com>
2025-11-27 10:48:01 +0100

    Done Task 0. Rain.

---
+++ rain/0-rain.py
@@ -0,0 +1,21 @@
+"""
+0-rain
+"""
+
+
+def rain(walls):
+    """Calculate how many square units of water will be retained."""
+    if not walls:
+        return 0
+
+    n = len(walls)
+    water = 0
+
+    for i in range(n):
+        left_max = max(walls[:i+1])
+
+        right_max = max(walls[i:])
+
+        water += min(left_max, right_max) - walls[i]
+
+    return water
