c9f91b177be81bf701ad85290d55cc2e2111a2ff
cableyesto <dialkins@tutamail.com>
2026-03-31 19:03:13 +0200

    Done task 0. Pascal's Triangle.

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists representing Pascal's triangle of n rows"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # First row
+
+    for i in range(1, n):
+        row = [1]  # First element is always 1
+        # Generate middle elements
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)  # Last element is always 1
+        triangle.append(row)
+
+    return triangle
